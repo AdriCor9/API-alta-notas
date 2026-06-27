02bb401a6bbcd99a2cd0cbe433c660258e961553
Adri Cor <adriandragon512@gmail.com>
2026-06-27 16:12:30 -0600

    configuracion var entorno cred google v2

--- src/main/java/org/adri/apinotaalta/config/FirebaseConfig.java
+++ src/main/java/org/adri/apinotaalta/config/FirebaseConfig.java
@@ -14,30 +14,31 @@ import java.util.Base64;
 @Configuration
 public class FirebaseConfig {
     @Bean
-    public FirebaseApp firebaseApp() throws IOException{
+    public FirebaseApp firebaseApp() throws IOException {
         GoogleCredentials credentials;
 
         String firebaseConfigBase64 = System.getenv("FIREBASE_SERVICE_ACCOUNT_BASE64");
 
-        if(firebaseConfigBase64 !=null && !firebaseConfigBase64.isBlank()){
+        if (firebaseConfigBase64 != null && !firebaseConfigBase64.isBlank()) {
             byte[] decodedConfig = Base64.getDecoder().decode(firebaseConfigBase64);
-            credentials = GoogleCredentials.fromStream(new ByteArrayInputStream(decodedConfig));
-        }
-        else {
+            credentials = GoogleCredentials.fromStream(
+                    new ByteArrayInputStream(decodedConfig)
+            );
+        } else {
             FileInputStream serviceAccount = new FileInputStream(
                     "src/main/resources/firebase-service-account.json"
             );
             credentials = GoogleCredentials.fromStream(serviceAccount);
         }
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
+        FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(credentials)
                 .build();
 
-        if(FirebaseApp.getApps().isEmpty()){
+        if (FirebaseApp.getApps().isEmpty()) {
             return FirebaseApp.initializeApp(options);
         }
 
-        return FirebaseApp.initializeApp(options);
+        return FirebaseApp.getInstance();
     }
 }
